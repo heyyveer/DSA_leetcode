513fca015544dace433d0ac0fab1b303bc7a6d6f
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-06-29 00:03:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (65.63%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -8,7 +8,6 @@ public:
         int addedS = 0;
         int addedT = 0;
 
-        // Process s
         for (int i = 0; i < n; i++) {
             if (s[i] != '#') {
                 s.push_back(s[i]);
@@ -19,7 +18,6 @@ public:
             }
         }
 
-        // Process t
         for (int i = 0; i < m; i++) {
             if (t[i] != '#') {
                 t.push_back(t[i]);
