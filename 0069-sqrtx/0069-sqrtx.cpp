fb0a4c383fc26f1701bbb9daa183c9892197d572
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-07-02 17:21:21 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.68%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        int ans=0;
+        if(high<=0){
+            return 0;
+        }
+        if(high==1){
+            return 1;
+        }
+        while(low<=high){
+            int mid = low + (high - low) / 2;
+            long long square = 1LL * mid * mid;
+            if(square==x){
+                return mid;
+            }
+            else if(square<x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
