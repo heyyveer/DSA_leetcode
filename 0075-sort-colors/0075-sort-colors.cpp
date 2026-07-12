566f28d259fbb7835486afcbd33093b73d2292f2
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-07-12 13:35:53 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.1%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // int left=0;
-        // int right=nums.size()-1;
-        // while(left<right){
-        //     if(nums[right]==2){
-        //         right--;
-        //     }
-        //     else if(nums[left]==0){
-        //         left++;
-        //     }
-        //     else if(nums[left]==2 && nums[right]==0){
-        //         swap(nums[left],nums[right]);
-        //         left++;
-        //         right--;
-        //     }
-        //     else if(nums[right]==1 && nums[left]==0){
-        //         left++;
-        //     }
-        //     else if(nums[right]==1 && nums[left]==2){
-        //         swap(nums[left],nums[right]);
-        //         right--;
-        //     }
-        // }
-        // return;
-        sort(nums.begin(),nums.end());
+        int left=0;
+        int right=nums.size()-1;
+        int mid=0;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[left],nums[mid]);
+                left++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[right]);
+                right--;
+            }
+        }
     }
 };
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int low = 0;
+//         int mid = 0;
+//         int high = nums.size() - 1;
+//         while (mid <= high) {
+//             if (nums[mid] == 0) {
+//                 swap(nums[low], nums[mid]);
+//                 low++;
+//                 mid++;
+//             }
+//             else if (nums[mid] == 1) {
+//                 mid++;
+//             }
+//             else {
+//                 swap(nums[mid], nums[high]);
+//                 high--;
+//             }
+//         }
+//     }
+// };
