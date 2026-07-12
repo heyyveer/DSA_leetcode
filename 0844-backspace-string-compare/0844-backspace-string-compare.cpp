992d5de7295db027d9d12d5dc82b7c2d08601507
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-07-12 14:04:36 +0530

    Time: 0 ms (100%), Space: 8.4 MB (67.36%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-
-        int n = s.size();
-        int m = t.size();
-
-        int addedS = 0;
-        int addedT = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] != '#') {
-                s.push_back(s[i]);
-                addedS++;
-            } else if (addedS > 0) {
-                s.pop_back();
-                addedS--;
+        string a="";
+        string b="";
+        for(char ch: s){
+            if(ch!='#'){
+                a+=ch;
+            }
+            else if(!a.empty()){
+                a.pop_back();
             }
         }
-
-        for (int i = 0; i < m; i++) {
-            if (t[i] != '#') {
-                t.push_back(t[i]);
-                addedT++;
-            } else if (addedT > 0) {
-                t.pop_back();
-                addedT--;
+        for(char ch: t){
+            if(ch!='#'){
+                b+=ch;
+            }
+            else if(!b.empty()){
+                b.pop_back();
             }
         }
-
-        if (addedS != addedT)
-            return false;
-
-        for (int i = 0; i < addedS; i++) {
-            if (s[n + i] != t[m + i])
-                return false;
-        }
-
-        return true;
+        return a==b;
     }
 };
