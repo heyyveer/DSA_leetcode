89b6c7aee5394d401ac75bc010f1f89fff9740b1
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-07-23 19:26:31 +0530

    Time: 0 ms (100%), Space: 199 MB (28.13%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n <= 2)
+            return n;
+
+        int bits = 32 - __builtin_clz(n);
+
+        return 1 << bits;
+    }
+};
