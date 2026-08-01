eac1ef279439d82c05c7fc10aeae5587f1b84991
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-08-01 11:37:30 +0530

    Time: 0 ms (100%), Space: 10 MB (52.91%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(vector<int>& nums,int i,int j){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=INT_MIN){
+            return dp[i][j];
+        }
+        int left=nums[i]-solve(nums,i+1,j);
+        int right=nums[j]-solve(nums,i,j-1);
+        return dp[i][j]=max(left,right);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        dp.assign(n,vector<int>(n,INT_MIN));
+        return solve(nums,0,n-1)>=0;
+    }
+};
