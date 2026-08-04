041a95bb4aa2f1e598d68f1a09343f27698c12b6
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-08-04 13:26:06 +0530

    Time: 0 ms (100%), Space: 32.2 MB (91.09%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -5,15 +5,30 @@ public:
         int n = nums.size()-1;
         int min = nums[0] , max = nums[n];
         vector<int>ans;
-        for(int i = min ; i<=max ; i++){
-            for(int j = 0 ; j<=n ; j++){
-                if(i == nums[j]){
-                    break;
-                }
-                else if(i != nums[j] && j==n){
-                    ans.push_back(i);
+        //O(n^2)
+        // for(int i = min ; i<=max ; i++){
+        //     for(int j = 0 ; j<=n ; j++){
+        //         if(i == nums[j]){
+        //             break;
+        //         }
+        //         else if(i != nums[j] && j==n){
+        //             ans.push_back(i);
 
-                }
+        //         }
+        //     }
+        // }
+
+
+        //O(n)
+        int k = min;
+        int l = 0;
+        for(int i = min ;i<=max;i++){
+            if(nums[l] == k){
+                k++;
+                l++;
+            }else{
+                ans.push_back(k);
+                k++;
             }
         }
         return ans;
