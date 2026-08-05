c6b2648efda1d422f6d42ee908f9c6a26404a831
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-08-05 11:56:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.85%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int add(int a){
-        int sum=0;
-        while(a){
-            int k=a%10;
-            sum+=k;
-            a=a/10;
-        }
-        return sum;
-    }
+    // int add(int a){
+    //     int sum=0;
+    //     while(a){
+    //         int k=a%10;
+    //         sum+=k;
+    //         a=a/10;
+    //     }
+    //     return sum;
+    // }
 
     int addDigits(int num) {
-        while(num>=10){
-            num=add(num);
-        }
-        return num;
+        // while(num>=10){
+        //     num=add(num);
+        // }
+        // return num;
+        if(num==0) return 0;
+        return 1 + (num-1)%9;
     }
 };
