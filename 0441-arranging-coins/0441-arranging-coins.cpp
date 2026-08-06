252701dbad7ef206344a5ee24be90fb5594a49fd
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-08-06 10:46:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.87%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -12,21 +12,21 @@ public:
     // }
     int arrangeCoins(int n) {
         // int k = check(n);
-        long long left=0;
-        long long right=n;
-        int ans=0;
-        while(left<=right){
-            long long mid=left+(right-left)/2;
-            long long coins=mid*(mid+1)/2;
-            if(coins<=n){
-                ans=mid;
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
-        }
-        return ans;
-        // return (sqrt(8.0*n+1)-1)/2;
+        // long long left=0;
+        // long long right=n;
+        // int ans=0;
+        // while(left<=right){
+        //     long long mid=left+(right-left)/2;
+        //     long long coins=mid*(mid+1)/2;
+        //     if(coins<=n){
+        //         ans=mid;
+        //         left=mid+1;
+        //     }
+        //     else{
+        //         right=mid-1;
+        //     }
+        // }
+        // return ans;
+        return (sqrt(8.0*n+1)-1)/2;
     }
 };
