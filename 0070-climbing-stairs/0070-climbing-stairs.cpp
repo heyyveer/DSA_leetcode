dc06ee90b12db535086783f3779f9fae227ceeb0
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-09-10 18:34:11 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.42%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<long long> fib(n + 2);
+        fib[0]=0;
+        fib[1]=1;
+        for(int i =2;i<=n+1;i++){
+            fib[i]=fib[i - 1]+fib[i - 2];
+        }
+        return fib[n+1];
+    }
+};
