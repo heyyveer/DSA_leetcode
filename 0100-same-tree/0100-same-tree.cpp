40269126327ed49ccc4512f7a975b453c8960c17
Veer Tiwari <154729020+heyyveer@users.noreply.github.com>
2026-09-12 13:39:25 +0530

    Time: 0 ms (100%), Space: 13 MB (13.12%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -21,7 +21,7 @@ public:
         if(p->val != q->val){
             return false;
         }
-        bool left  = isSameTree(p->left,q->left);
+        bool left = isSameTree(p->left,q->left);
         bool right = isSameTree(p->right,q->right);
         return left && right;
     }
